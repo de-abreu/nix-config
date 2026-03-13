a68beaaf4880f2114ea26d7cd3dd681f8ca32687
Abreu <87032834+de-abreu@users.noreply.github.com>
2026-03-13 15:59:17 -0300

    feat(feat/nixvim): Added the Github Api-key to access GH through the CLI

--- home/features/programs/cli/fish/integrations/api-keys.nix
+++ home/features/programs/cli/fish/integrations/api-keys.nix
@@ -1,13 +1,18 @@
 { config, ... }:
 {
+  sops.secrets = {
+    "api-keys/deepseek" = { };
+    "api-keys/tavily" = { };
+    "api-keys/github" = { };
+  };
+
   programs.fish.shellInit =
     let
       inherit (config.sops) secrets;
     in
-    # fish
     ''
-      export DEEPSEEK_API_KEY=(cat ${secrets."api-keys/deepseek".path})
-      export TAVILY_API_KEY=(cat ${secrets."api-keys/tavily".path})
-      export GITHUB_TOKEN=(cat ${secrets."api-keys/github".path})
+      export DEEPSEEK_API_KEY (cat ${secrets."api-keys/deepseek".path})
+      export TAVILY_API_KEY (cat ${secrets."api-keys/tavily".path})
+      export GITHUB_TOKEN (cat ${secrets."api-keys/github".path})
     '';
 }
