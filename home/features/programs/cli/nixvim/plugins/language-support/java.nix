bd44e4c52184eca05cc4a1725fabfe1b603515f2
Abreu <87032834+de-abreu@users.noreply.github.com>
2026-04-26 00:14:39 +0300

    fix(nixvim): Added missing mason repository
    nvim-java manages its dependencies through mason, and adds to it its
    custom java repository. Still, to work properly, it needs mason to be
    set up with its default repository.

--- home/features/programs/cli/nixvim/plugins/language-support/java.nix
+++ home/features/programs/cli/nixvim/plugins/language-support/java.nix
@@ -2,9 +2,20 @@
 # nvim-java automatically manages: jdtls, lombok, java-test, java-debug-adapter, spring-boot-tools
 # NOTE: JDK should be provided by the project's dev flake or system configuration
 {
-  programs.nixvim.plugins.java = {
-    enable = true;
-    lazyLoad.settings.event = [ "DeferredUIEnter" ];
-    settings.jdk.auto_install = false;
+  programs.nixvim = {
+    plugins.java = {
+      enable = true;
+      lazyLoad.settings.event = [ "DeferredUIEnter" ];
+      settings.jdk.auto_install = false;
+    };
+
+    extraConfigLuaPre = ''
+      require('mason').setup({
+        registries = {
+          'github:nvim-java/mason-registry',
+          'github:mason-org/mason-registry',
+        },
+      })
+    '';
   };
 }
