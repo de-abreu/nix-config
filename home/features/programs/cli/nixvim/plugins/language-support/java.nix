4125771f6d3562cb64830768e8d74e62a412fb9b
Abreu <87032834+de-abreu@users.noreply.github.com>
2026-04-26 17:21:04 +0300

    fix(nixvim): Removed deprecation warning from nvim-java
    Temporary fix as the stable version of the plugin still requires nvim-lspconfig. When it is updated that will no longer be the case.

--- home/features/programs/cli/nixvim/plugins/language-support/java.nix
+++ home/features/programs/cli/nixvim/plugins/language-support/java.nix
@@ -1,21 +1,40 @@
 # INFO: Java language support using nvim-java
 # nvim-java automatically manages: jdtls, lombok, java-test, java-debug-adapter, spring-boot-tools
 # NOTE: JDK should be provided by the project's dev flake or system configuration
+{ pkgs, ... }:
 {
   programs.nixvim = {
+    # Suppress lspconfig deprecation warnings from nvim-java (still uses require('lspconfig'))
+    extraConfigLuaPre = ''
+      local orig_deprecate = vim.deprecate
+      vim.deprecate = function(name, banner, ...)
+        if (name and name:match("lspconfig")) or (banner and banner:match("lspconfig")) then
+          return
+        end
+        return orig_deprecate(name, banner, ...)
+      end
+    '';
+
     plugins.java = {
       enable = true;
       lazyLoad.settings.event = [ "DeferredUIEnter" ];
       settings.jdk.auto_install = false;
     };
-
-    extraConfigLuaPre = ''
-      require('mason').setup({
-        registries = {
-          'github:nvim-java/mason-registry',
-          'github:mason-org/mason-registry',
-        },
-      })
-    '';
   };
+
+  # nvim-java requires mason to manage its installation of java dependencies. This configuration sets up the proper registries
+  extra.lz-n.plugins = [
+    {
+      __unkeyed-1 = "mason.nvim";
+      event = [ "DeferredUIEnter" ];
+      after.__raw = ''
+        require('mason').setup({
+          registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        })
+      '';
+    }
+  ];
 }
