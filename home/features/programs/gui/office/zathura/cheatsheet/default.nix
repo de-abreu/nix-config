6cfa1d212d7866afa18423afedac82b2f65e7f13
Abreu <87032834+de-abreu@users.noreply.github.com>
2026-05-16 20:40:42 +0300

    fix(zathura): Added a space between "zathura" and the initial prompt

--- home/features/programs/gui/office/zathura/cheatsheet/default.nix
+++ home/features/programs/gui/office/zathura/cheatsheet/default.nix
@@ -7,7 +7,7 @@ in
   config = mkIf (navi.enable && wezterm.enable && zathura.enable) {
     programs = {
       zathura.mappings."H" = ''
-        feedkeys ":exec ${getExe wezterm.package} --config enable_tab_bar=false --config 'window_close_confirmation=\"NeverPrompt\"' start --class floating -- ${getExe navi.package} --query zathura<Return>"
+        feedkeys ":exec ${getExe wezterm.package} --config enable_tab_bar=false --config 'window_close_confirmation=\"NeverPrompt\"' start --class floating -- ${getExe navi.package} --query \"zathura \"<Return>"
       '';
       navi.settings.cheats.paths = [ (toString ./cheat) ];
     };
